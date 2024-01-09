91a9861fb9136c3e430a58de76c1045a0cd9f57f
Jesper Milton <jm224an@student.lnu.se>
2024-01-09 12:38:33 +0100

    tus

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ let bakG;
 let b1;
 let b2;
 let b3;
+let text;
 
 function init() {
-    bakG = document.querySelector("#middle .mid_layer")
+    bakG = document.querySelector("#middle .mid_layer");
+    text = document.querySelector("#textChange");
 
     b1 = document.getElementById("nav-1");
     b1.addEventListener("click", function () {
@@ -27,9 +29,19 @@ window.addEventListener("load", init);
 function changeBack(color) {
     bakG.style.transition = "background-color 1s ease";
     bakG.style.backgroundColor = color;
+    textChange(color);
 
     setTimeout(function() {
         bakG.style.transition = "";
     }, 500);
 }
 
+function textChange(color) {
+    if (color === "blue") {
+        text.innerText = "Ändrar till blått";
+    } else if (color === "red") {
+        text.innerText = "Ändrar till rött";
+    } else if (color === "yellow") {
+        text.innerText = "Ändrar till gult";
+    }
+}
